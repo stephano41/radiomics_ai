c72dcc133bfced8462daddee383d2db90d3878cb
stephano41 <78075921+stephano41@users.noreply.github.com>
2024-03-28 00:47:19 +1030

    converting dicom to nifti

--- scratch.py
+++ scratch.py
@@ -1,7 +1,16 @@
-import torch
-from src.models.autoencoder.fmcib_model import load_fmcib_model
+from src.dataset.custom_dicom2nifti import dicomdir2nii, get_dicom_meta, get_dicom_meta, get_dicomdir_meta
+
+dataset_meta = dicomdir2nii('./data/sarcoma_dicom', './data/sarcoma_nifti', series_description_filter=['T2.*AX','DIFF.*ADC'], codependent=True, codependent_tolerance=0.5)
+dataset_meta.to_csv('data/sarcoma_meta.csv')
+# print(dataset_meta)
 
 
-model = load_fmcib_model(save_path='outputs/pretrained_models')
-
-ip = torch.rand(1,1,96,96,96)
+# print(get_dicomdir_meta('data/sarcoma_dicom/SARC_00004/DICOMDIR'))
+# inspect_dicom('data/sarcoma_dicom/SARC_00002/DICOMDIR')
+# sitk_inspect_dicom('data/sarcoma_dicom/SARC_00001/DICOM/PA000003/ST000001/SE000001')
+# dicommetas = []
+# for dicomdir_file in Path('data/sarcoma_dicom').rglob("DICOMDIR"):
+#     dicommetas.extend(get_dicomdir_meta(str(dicomdir_file)))
+#
+# meta_csv = pd.DataFrame(dicommetas)
+# meta_csv.to_csv('outputs/test_sarcoma_csv.csv')
